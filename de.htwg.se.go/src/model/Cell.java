35338ef95827ecc091a2e0937876ec250e9437d7
Michael Knoch <michael-knoch@gmx.net>
2013-10-22 14:51:34 +0200

    implemented cell class

--- de.htwg.se.go/src/model/Cell.java
+++ de.htwg.se.go/src/model/Cell.java
@@ -4,14 +4,28 @@ public class Cell {
 	private int status;
 
 	public Cell() {
-		status = 0;
+		setStatus(0);
 	}
 	
 	
 	public Cell(String x) {
 		if (x.equals("w")) {
-			status = 1;
-		} else if (x.equals("b"));
+			setStatus(1);
+		} else if (x.equals("b")) {
+			setStatus(2);
+		} else {
+			setStatus(0);
+		}
+	}
+
+
+	public int getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(int status) {
+		this.status = status;
 	}
 
 }
