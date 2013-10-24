89e4fc94c51d02083eb54335e1931755022fbba4
Michael Knoch <michael-knoch@gmx.net>
2013-10-24 18:29:25 +0200

    removed some warnings, we call it a day!

--- de.htwg.se.go/src/de/htwg/go/go.java
+++ de.htwg.se.go/src/de/htwg/go/go.java
@@ -1,21 +1,17 @@
 package de.htwg.go;
 
-import java.util.Scanner;
+
 
 import de.htwg.go.aview.TextUI;
 import de.htwg.go.controller.impl.GoController;
 
 public class go {
-	private static Scanner scanner;
+
 	
 	public static void main(final String args[]) {
 
 		GoController controller = new GoController();
-		TextUI textui = new TextUI(controller);
-		
+		new TextUI(controller);
 		controller.setStone(1, 1, "w");
-
-		scanner = new Scanner(System.in);
-		scanner.hasNext()
 	}
 }
