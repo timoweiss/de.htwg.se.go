21a59351f0cd3915e1e403be29ecf0ba3a656e27
Michael Knoch <michael-knoch@gmx.net>
2013-11-08 19:13:18 +0100

    removed system.out.println

--- de.htwg.se.go/src/de/htwg/go/aview/TextUI.java
+++ de.htwg.se.go/src/de/htwg/go/aview/TextUI.java
@@ -1,5 +1,7 @@
 package de.htwg.go.aview;
 
+import java.util.logging.Logger;
+
 import de.htwg.go.controller.IGoController;
 import de.htwg.go.util.observer.Event;
 import de.htwg.go.util.observer.IObserver;
@@ -7,6 +9,8 @@ import de.htwg.go.util.observer.IObserver;
 public class TextUI implements IObserver {
 
 	private IGoController controller;
+	private Logger logger = Logger.getLogger("de.htwg.go");
+	private String newLine = System.getProperty("line.separator");
 
 	public TextUI(IGoController controller) {
 		this.controller = controller;
@@ -19,34 +23,30 @@ public class TextUI implements IObserver {
 	}
 
 	private void printTui() {
-		System.out.println(controller.tuiToString());
-		System.out.println(controller.getStatus());
-		System.out.println();
+		logger.info(newLine + controller.tuiToString());
+		logger.info(newLine + controller.getStatus());
 	}
 
 	public boolean inputLine(String line) {
 		boolean operate = true;
-		
+
 		if (line.equals("-quit")) {
 			operate = false;
-			System.out.println("bye");
+			logger.info("bye");
 			return false;
 		}
-		
-		/*
-		switch (line) {
-		case "-quit":
-			operate = false;
-			System.out.println("Bye");
-			return false;
-		}*/
 
-		//if the input has "123" set a Stone at 1, 2 with status 3
+		/*
+		 * switch (line) { case "-quit": operate = false;
+		 * System.out.println("Bye"); return false; }
+		 */
+
+		// if the input has "123" set a Stone at 1, 2 with status 3
 		if (line.matches("[0-9][0-9]")) {
 			controller.setStone(Character.getNumericValue(line.charAt(0)),
 					Character.getNumericValue(line.charAt(1)));
 		} else {
-			System.out.println("incorrect command");
+			logger.info("incorrect command");
 		}
 
 		return operate;
