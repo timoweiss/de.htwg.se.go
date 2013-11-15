1b8c8034f2d6c76f505b022ea42f8cbf9f5829a8
Michael Knoch <michael-knoch@gmx.net>
2013-11-15 12:18:49 +0100

    added score functunality

--- de.htwg.se.go/src/de/htwg/go/model/Player.java
+++ de.htwg.se.go/src/de/htwg/go/model/Player.java
@@ -2,17 +2,29 @@ package de.htwg.go.model;
 
 public class Player {
 	private String name;
+	private int score;
 
 	
 	public Player() {
 		this.name = "unknown";
+		score = 0;
 	}
 	
 	public Player(String name) {
 		this.name = name;
+		score = 0;
 	}
 
 	public String getName() {
 		return name;
+	}
+	
+	public int addPoints(int scoretoAdd) {
+		score += scoretoAdd;
+		return this.score;
+	}
+	
+	public int getScore() {
+		return this.score;
 	}
 }
--- de.htwg.se.go/test/de/htwg/go/model/PlayerTest.java
+++ de.htwg.se.go/test/de/htwg/go/model/PlayerTest.java
@@ -9,17 +9,41 @@ public class PlayerTest {
 
 	Player unknownPlayer;
 	Player knownPlayer;
-	
+
 	@Before
 	public void setUp() throws Exception {
-	unknownPlayer = new Player();
-	knownPlayer = new Player("known");
+		unknownPlayer = new Player();
+		knownPlayer = new Player("known");
 	}
 
 	@Test
 	public void testGetName() {
 		assertEquals("unknown", unknownPlayer.getName());
 		assertEquals("known", knownPlayer.getName());
+	}
+
+	@Test
+	public void testaddScore() {
+		unknownPlayer.addPoints(500);
+		unknownPlayer.addPoints(5);
+
+		knownPlayer.addPoints(501);
+		knownPlayer.addPoints(9);
+
+		assertEquals(505, unknownPlayer.addPoints(0));
+		assertEquals(510, knownPlayer.addPoints(0));
+	}
+
+	@Test
+	public void testgetScore() {
+		unknownPlayer.addPoints(500);
+		unknownPlayer.addPoints(5);
+
+		knownPlayer.addPoints(501);
+		knownPlayer.addPoints(9);
+
+		assertEquals(505, unknownPlayer.getScore());
+		assertEquals(510, knownPlayer.getScore());
 	}
 
 }
