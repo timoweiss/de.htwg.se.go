78eb896e9903025dad147bdeb3f7d630caba54bf
Michael Knoch <michael-knoch@gmx.net>
2014-01-10 16:28:00 +0100

    testing

---
+++ de.htwg.se.go/test/de/htwg/go/model/ObservableTest.java
@@ -0,0 +1,58 @@
+package de.htwg.go.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.htwg.go.util.observer.Event;
+import de.htwg.go.util.observer.IObserver;
+import de.htwg.go.util.observer.Observable;
+
+
+
+public class ObservableTest {
+
+	private boolean ping=false;
+    private TestObserver testObserver;
+    private Observable testObservable;
+    
+    class TestObserver implements IObserver {
+            //@Override
+            public void update(Event e) {
+                    ping=true;
+            }
+            
+    }
+
+    @Before
+    public void setUp() throws Exception {
+            testObserver = new TestObserver();
+            testObservable = new Observable();
+            testObservable.addObserver(testObserver);
+    }
+
+    @Test
+    public void testNotify() {
+            assertFalse(ping);
+            testObservable.notifyObservers();
+            assertTrue(ping);
+    }
+    
+    @Test
+    public void testRemove() {
+            assertFalse(ping);
+            testObservable.removeObserver(testObserver);
+            testObservable.notifyObservers();
+            assertFalse(ping);
+    }
+    
+    @Test
+    public void testRemoveAll() {
+            assertFalse(ping);
+            testObservable.removeAllObservers();
+            testObservable.notifyObservers();
+            assertFalse(ping);
+    }
+
+}
