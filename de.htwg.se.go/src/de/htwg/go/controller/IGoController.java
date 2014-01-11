3c997e5ffb42e45617ef3f833d7b8bae1a29ddd4
Michael Knoch <michael-knoch@gmx.net>
2014-01-11 12:43:33 +0100

    addded javadoc

--- de.htwg.se.go/src/de/htwg/go/controller/IGoController.java
+++ de.htwg.se.go/src/de/htwg/go/controller/IGoController.java
@@ -4,31 +4,68 @@ import de.htwg.go.util.observer.IObservable;
 
 public interface IGoController extends IObservable {
 
-	//Creates the Gamefield to play
+	/**
+	 * creates a gamefield to play
+	 */
 	void createField();
 
-	//returns the tui in String format
+
+	
+	/**
+	 * returns the tui in String format
+	 * @return tui string
+	 */
 	String tuiToString();
 
-	//Sets a stone and knows, whos next
+
+	
+	/**
+	 * Sets a stone and returns if the stoneset workd
+	 * @param x x-coordinate
+	 * @param y y-coordinate
+	 * @return boolean
+	 */
 	boolean setStone(int x, int y);
 	
-	//Sets a stone and wants to know which player is next
+	/**
+	 * Sets a stone and wants to know which player is next
+	 */
 	void setStone(int x, int y, int status);
 
-	//returns the statusline
+	
+	/**
+	 * returns the gamestatus
+	 * @return string
+	 */
 	String getStatus();
 	
 	//returns the Score from whitePlayer
+	
+	/**
+	 * returns the Score of white Player
+	 * @return int
+	 */
 	int getwhitePlayerScore();
 	
-	//returns the Score from whitePlayer
+	/**
+	 * returns the Score of black Player
+	 * @return int
+	 */
 	int getblackPlayerScore();
 
-	//returns the status of a cell
+	/**
+	 * returns the cell status
+	 * @param x-coordinate
+	 * @param y-coordinate
+	 * @return status in int
+	 */
 	int getCellStatus(int x, int y);
 	
-	//return if white is next or not
+	
+	/**
+	 * returns which player is next
+	 * @return string
+	 */
 	String getNext();
 	
 }
