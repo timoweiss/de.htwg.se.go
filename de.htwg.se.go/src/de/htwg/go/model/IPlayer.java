fd34a57e314ecb1769e6428ac721cbdfd855ca2b
Michael Knoch <michael-knoch@gmx.net>
2014-01-11 12:05:29 +0100

    sonar improvments

--- de.htwg.se.go/src/de/htwg/go/model/IPlayer.java
+++ de.htwg.se.go/src/de/htwg/go/model/IPlayer.java
@@ -5,19 +5,19 @@ public interface IPlayer {
 	 *  returns the name of a Player
 	 * @return name as string
 	 */
-	public String getName();
+	String getName();
 	
 	/**
 	 * returns the score of a player
 	 * @return score in int
 	 */
-	public int getScore();
+	int getScore();
 
 	/**
 	 * adds some score to a player and returns the new score
 	 * @param the score to add
 	 * @return the new score as int
 	 */	
-	public int addScore(int scoreToAdd);
+	int addScore(int scoreToAdd);
 
 }
--- de.htwg.se.go/src/de/htwg/go/model/IScore.java
+++ de.htwg.se.go/src/de/htwg/go/model/IScore.java
@@ -6,11 +6,11 @@ public interface IScore {
 	 * @param the score to add in int
 	 * @return the new score
 	 */
-	public int addPoints(int scoretoAdd);
+	int addPoints(int scoretoAdd);
 	
 	/**
 	 * returns the score value
 	 * @return the score value
 	 */
-	public int getScore();
+	int getScore();
 }
