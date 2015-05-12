6e8714c4280c9b0d5cdeeeab7ab223c795ad5942
Timo Weiß <info@timo-weiss.com>
2015-05-12 15:32:59 +0200

    add couchdbconnector

--- de.htwg.se.go/src/de/htwg/go/persistence/couchdb/GameFieldCouchdbDAO.java
+++ de.htwg.se.go/src/de/htwg/go/persistence/couchdb/GameFieldCouchdbDAO.java
@@ -1,8 +1,9 @@
 package de.htwg.go.persistence.couchdb;
 
 import de.htwg.go.persistence.IGameFieldDAO;
+import org.ektorp.CouchDbConnector;
 
 public class GameFieldCouchdbDAO implements IGameFieldDAO {
 
-    
+    private CouchDbConnector db = null;
 }
