57673fa51608ca716d6b45bdc15a8fa731e186d8
Timo Weiß <info@timo-weiss.com>
2015-06-24 21:20:26 +0200

    do some more rediculous java things

--- de.htwg.se.go/src/de/htwg/go/persistence/couchdb/Util.java
+++ de.htwg.se.go/src/de/htwg/go/persistence/couchdb/Util.java
@@ -1,6 +1,10 @@
 package de.htwg.go.persistence.couchdb;
 
+import de.htwg.go.model.ICell;
 import de.htwg.go.model.IGameField;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by timoweiss on 24/06/15.
@@ -16,6 +20,17 @@ public class Util {
         persistentGameField.setPass(gameField.passed());
         persistentGameField.setWhiteIsNext(gameField.isWhiteIsNext());
 
+        Set<PersistenceCell> setPersistenceCell = new HashSet<PersistenceCell>();
+        for (ICell current: gameField.getBlackList()) {
+            PersistenceCell persistenceCell = new PersistenceCell();
+
+            persistenceCell.setChecked(current.isChecked());
+            persistenceCell.setCoords(current.getCoords());
+            persistenceCell.setStatus(current.getStatus());
+
+            setPersistenceCell.add(persistenceCell);
+        }
+        persistentGameField.setBlackList(setPersistenceCell);
         return null;
     }
 }
