6044f078b6f500be6646e437833129132ccb55cb
Timo Weiß <info@timo-weiss.com>
2015-06-24 13:00:37 +0200

    add some injection

--- de.htwg.se.go/src/de/htwg/go/GoModule.java
+++ de.htwg.se.go/src/de/htwg/go/GoModule.java
@@ -5,6 +5,7 @@ import com.google.inject.AbstractModule;
 import de.htwg.go.controller.IGoController;
 import de.htwg.go.model.IPlayer;
 import de.htwg.go.model.IScore;
+import de.htwg.go.persistence.IGameFieldDAO;
 
 public class GoModule extends AbstractModule {
 	@Override
@@ -16,5 +17,9 @@ public class GoModule extends AbstractModule {
 
 		bind(IGoController.class).to(
 				de.htwg.go.controller.impl.GoController.class);
+
+        bind(IGameFieldDAO.class).to(de.htwg.go.persistence.db4o.GameFieldDb4oDAO.class);
+        //bind(IGameFieldDAO.class).to(de.htwg.go.persistence.db4o.GameFieldDb4oDAO.class);
+        //bind(IGameFieldDAO.class).to(de.htwg.go.persistence.db4o.GameFieldDb4oDAO.class);
 	}
 }
