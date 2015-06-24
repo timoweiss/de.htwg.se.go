6de7484da015cd2f0de4c2fc60bcce9a4a872bb9
Timo Weiß <info@timo-weiss.com>
2015-06-24 21:31:05 +0200

    generate getter setter

--- de.htwg.se.go/src/de/htwg/go/persistence/couchdb/PersistenceScore.java
+++ de.htwg.se.go/src/de/htwg/go/persistence/couchdb/PersistenceScore.java
@@ -4,4 +4,13 @@ package de.htwg.go.persistence.couchdb;
  * Created by timoweiss on 24/06/15.
  */
 public class PersistenceScore {
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
