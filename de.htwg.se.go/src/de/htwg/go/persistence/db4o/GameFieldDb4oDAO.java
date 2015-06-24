dd12797a667b4876dc2f0848e97d4a6ef9660d45
Timo Weiß <info@timo-weiss.com>
2015-06-24 12:19:31 +0200

    java sucks

--- de.htwg.se.go/src/de/htwg/go/persistence/db4o/GameFieldDb4oDAO.java
+++ de.htwg.se.go/src/de/htwg/go/persistence/db4o/GameFieldDb4oDAO.java
@@ -3,10 +3,17 @@ package de.htwg.go.persistence.db4o;
 import de.htwg.go.model.IGameField;
 import de.htwg.go.persistence.IGameFieldDAO;
 
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.query.Query;
+
 /**
  * Created by timoweiss on 24/06/15.
  */
 public class GameFieldDb4oDAO implements IGameFieldDAO {
+
+    private ObjectContainer db;
+
     @Override
     public void saveGame(IGameField gameField) {
 
