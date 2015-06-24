43d3f321b05f99aa57e35af52d3cbe8b17d41967
Timo Weiß <info@timo-weiss.com>
2015-06-24 11:38:37 +0200

    init

--- de.htwg.se.go/src/de/htwg/go/persistence/IGameFieldDAO.java
+++ de.htwg.se.go/src/de/htwg/go/persistence/IGameFieldDAO.java
@@ -3,4 +3,15 @@ package de.htwg.go.persistence;
 import de.htwg.go.model.IGameField;
 
 public interface IGameFieldDAO {
+
+    void saveGame(IGameField gameField);
+
+    IGameField getGameById(final String id);
+
+    void deleteGameById(final String id);
+
+    void updateGameById(final String id, IGameField gameField);
+
+    boolean contains(final String id);
+
 }
