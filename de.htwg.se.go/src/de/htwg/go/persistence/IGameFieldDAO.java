8f436c9c00c533fa9af2e59311bdad898e1bd077
Timo Weiß <info@timo-weiss.com>
2015-06-24 19:41:28 +0200

    impl interface and db4o

--- de.htwg.se.go/src/de/htwg/go/persistence/IGameFieldDAO.java
+++ de.htwg.se.go/src/de/htwg/go/persistence/IGameFieldDAO.java
@@ -2,6 +2,8 @@ package de.htwg.go.persistence;
 
 import de.htwg.go.model.IGameField;
 
+import java.util.List;
+
 public interface IGameFieldDAO {
 
     void saveGame(IGameField gameField);
@@ -9,6 +11,10 @@ public interface IGameFieldDAO {
     IGameField getGameById(final String id);
 
     void deleteGameById(final String id);
+
+    List<IGameField> getAllGames();
+
+    public void closeDB();
 
     void updateGameById(final String id, IGameField gameField);
 
--- de.htwg.se.go/src/de/htwg/go/persistence/db4o/GameFieldDb4oDAO.java
+++ de.htwg.se.go/src/de/htwg/go/persistence/db4o/GameFieldDb4oDAO.java
@@ -2,12 +2,12 @@ package de.htwg.go.persistence.db4o;
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
+import com.db4o.query.Predicate;
 import de.htwg.go.model.IGameField;
 import de.htwg.go.persistence.IGameFieldDAO;
 
-/**
- * Created by timoweiss on 24/06/15.
- */
+import java.util.List;
+
 public class GameFieldDb4oDAO implements IGameFieldDAO {
 
     private ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "db4o_go.data");
@@ -18,22 +18,50 @@ public class GameFieldDb4oDAO implements IGameFieldDAO {
     }
 
     @Override
-    public IGameField getGameById(String id) {
-        return null;
+    public IGameField getGameById(final String id) {
+        List<IGameField> games = db.query(new Predicate<IGameField>() {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public boolean match(IGameField game) {
+                return (game.getId().equals(id));
+            }
+        });
+
+        if (games.isEmpty()) {
+            // no game found
+            return null;
+        }
+
+        // return the retrieved game.
+        return games.get(0);
     }
 
     @Override
     public void deleteGameById(String id) {
+        db.delete(getGameById(id));
+    }
 
+    @Override
+    public List<IGameField> getAllGames() {
+        return db.query(IGameField.class);
+    }
+
+    @Override
+    public void closeDB() {
+        db.close();
     }
 
     @Override
     public void updateGameById(String id, IGameField gameField) {
-
+        db.delete(gameField.getId());
+        saveGame(gameField);
     }
 
     @Override
     public boolean contains(String id) {
-        return false;
+        IGameField game = getGameById(id);
+        return (game != null);
     }
 }
