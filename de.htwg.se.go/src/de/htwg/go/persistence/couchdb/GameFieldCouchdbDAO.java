7517ce15b01faa115f683ce2e5dc1ae8efe71cd2
Timo Weiß <info@timo-weiss.com>
2015-06-24 11:38:10 +0200

    things

--- de.htwg.se.go/src/de/htwg/go/persistence/couchdb/GameFieldCouchdbDAO.java
+++ de.htwg.se.go/src/de/htwg/go/persistence/couchdb/GameFieldCouchdbDAO.java
@@ -1,9 +1,35 @@
 package de.htwg.go.persistence.couchdb;
 
+import de.htwg.go.model.IGameField;
 import de.htwg.go.persistence.IGameFieldDAO;
 import org.ektorp.CouchDbConnector;
 
 public class GameFieldCouchdbDAO implements IGameFieldDAO {
 
     private CouchDbConnector db = null;
+
+    @Override
+    public void saveGame(IGameField gameField) {
+
+    }
+
+    @Override
+    public IGameField getGameById(String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteGameById(String id) {
+
+    }
+
+    @Override
+    public void updateGameById(String id, IGameField gameField) {
+
+    }
+
+    @Override
+    public boolean contains(String id) {
+        return false;
+    }
 }
